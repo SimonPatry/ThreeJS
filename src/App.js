c2650df229baeb6901c191f49493d61c82230c18
simon* <simonpatry12@gmail.com>
2022-10-28 18:38:19 +0200

    pushed first imports to render gld files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from './component/Model/Model';
+import './App.css';
+
+const App = () => {
+  
+}
+
+export default App;
